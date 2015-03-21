446458f3e66f7b3511ad2aa0161482f2a383ef0c
elfelfmooncake <elfelfmooncake@users.noreply.github.com>
2015-03-21 16:08:15 -0700

    Create strings.js

---
+++ strings.js
@@ -0,0 +1,7 @@
+var all_strings = {
+  "1": "Hello and welcome to my world",
+  "2": "The room is dark. What will you do?",
+  "3": "Turn on the lights",
+  "4": "Scream for mommy",
+  "5": "Call on the god  of light"
+};
